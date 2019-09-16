7d18e420aa3c106567efd9c015150c4ec717f624
root <root@perf98.perf.lab.eng.bos.redhat.com>
2019-09-15 22:06:17 -0400

    update run.sh for trafficgen run example

--- rt-container/run.sh
+++ rt-container/run.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 docker run  --rm --device=/dev/cpu_dma_latency:/dev/cpu_dma_latency --cap-add=SYS_RAWIO --cap-add=SYS_NICE --cap-add=IPC_LOCK -v /tmp/results:/cyclictest_results -e DURATION=2m cscojianzhan/rt-cyclictest
+
+docker run -it --rm --privileged  -v /sys:/sys -v /dev:/dev -v /lib/modules:/lib/modules --cpuset-cpus 2,4,6,8 docker.io/cscojianzhan/trafficgen bash
+
